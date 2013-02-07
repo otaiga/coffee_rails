c5e4a9eea479320536f20b45a0adf3143408958f
otaiga <karl.turner5@gmail.com>
2013-02-07 07:49:30 +0000

    added links in main view for editing items

--- app/controllers/mains_controller.rb
+++ app/controllers/mains_controller.rb
@@ -1,12 +1,8 @@
 class MainsController < ApplicationController
   before_filter :authenticate_user!
 
-# TODO (tests for models first), Group these by category (food and drink)
   def index
-    @food_menu = FoodMenu.all
-    @drinks_menu = DrinkMenu.all
-    @events = Event.all
-    @openning_times = OpeningTime.all
+
   end
 
 end
