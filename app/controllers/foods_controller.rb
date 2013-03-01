3346517c5c6113b6d8ebd5bb185f2730ec6b52ed
otaiga <karl.turner5@gmail.com>
2013-03-01 14:39:52 +0000

    added some functions to add food item, etc

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -5,15 +5,20 @@ class FoodsController < ApplicationController
   end
 
   def edit
-
+    @food_item = FoodMenu.find(params[:id])
   end
 
   def new
-
+    @food_item = FoodMenu.new
   end
 
   def create
-
+    @food_item = FoodMenu.new(params[:food_menu])
+    if @food_item.save
+      redirect_to foods_path
+    else
+      redirect_to foods_path
+    end
   end
 
   def update
