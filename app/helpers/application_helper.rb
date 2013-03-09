66807b21696c31ab133370ddf672f6e79411895c
otaiga <karl.turner5@gmail.com>
2013-03-09 08:53:00 +0000

    added back links and price helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
+# encoding: utf-8
+
+#Had to add the above encoding option in order to use
+#multibyte char ASCII (£)
+
 module ApplicationHelper
+
+  def f_price(value)
+    "£" + "%.2f" % value.to_f
+  end
+
 end
