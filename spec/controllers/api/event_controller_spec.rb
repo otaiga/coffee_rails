fe419f04ff441c24be8008a59ec6412ace31e47e
otaiga <karl.turner5@gmail.com>
2013-05-11 20:42:43 +0100

    added new tests for api_key

--- spec/controllers/api/event_controller_spec.rb
+++ spec/controllers/api/event_controller_spec.rb
@@ -1,6 +1,24 @@
 require 'spec_helper'
 
 describe Api::EventController do
+
+  context "api checks" do
+
+    it "should check for presence of an api key" do
+      get :index, :format => :json
+      expected_msg = "HTTP Token: Access denied."
+      JSON.parse(response.body)["errors"].first.should == expected_msg
+    end
+
+    it "should check for correct api key" do
+      request.env['HTTP_X_API_TOKEN'] = "wrong_key"
+      get :index, :format => :json
+      expected_msg = "HTTP Token: Access denied."
+      JSON.parse(response.body)["errors"].first.should == expected_msg
+    end
+  end
+
+  context "with api" do
     before(:all) do
       @test_data = {
         title: "test event",
@@ -9,17 +27,18 @@ describe Api::EventController do
       @token = "TestAPIKey"
     end
 
-  it "should check for json in request from base" do
-    request.env['HTTP_X_API_TOKEN'] = @token
-    get :index
-    expected_msg = "Sorry the request must be in json"
-    JSON.parse(response.body)["errors"].first.should == expected_msg
-  end
+    it "should check for json in request from base" do
+      request.env['HTTP_X_API_TOKEN'] = @token
+      get :index
+      expected_msg = "Sorry the request must be in json"
+      JSON.parse(response.body)["errors"].first.should == expected_msg
+    end
 
-  it "should provide a json response of food items" do
-    Event.create!(@test_data)
-    request.env['HTTP_X_API_TOKEN'] = @token
-    get :index, :format => :json
-    JSON.parse(response.body).should == {"test_event"=>"test data"}
+    it "should provide a json response of food items" do
+      Event.create!(@test_data)
+      request.env['HTTP_X_API_TOKEN'] = @token
+      get :index, :format => :json
+      JSON.parse(response.body).should == {"test_event"=>"test data"}
+    end
   end
 end
