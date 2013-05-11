926526155f85d3f9252a0d71fdea3f2a1fb1b4c4
otaiga <karl.turner5@gmail.com>
2013-05-11 19:59:19 +0100

    added api key for production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,4 +64,7 @@ CoffeeServer::Application.configure do
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
+
+  # API key
+  config.api_token = ENV['API_KEY']
 end
